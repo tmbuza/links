fe61c6285a8158d72baa93ba9f5eb7eca45e08c6
tmbuza <ndelly@gmail.com>
2022-08-22 13:17:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,16 +7,17 @@ load_css()
 col1, col2, col3 = st.columns(3)
 col2.image(Image.open('dp.png'))
 
-st.header('Chanin Nantasenamat, Ph.D.')
+st.header('Teresia Mrema Buza, Ph.D.')
 
-st.info('Developer Advocate, Content Creator and ex-Professor with an interest in Data Science and Bioinformatics')
+st.info('Self-Trained Developer, Data Scientist and Multi-Omics Bioinformatics Scientist')
 
 icon_size = 20
 
-st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
-st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
-st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
-st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
-st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
-st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
-st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
+# st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
+# st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
+# st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
+# st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
+st_button('streamlit', 'https://tmbuza-streamlit-example-streamlit-app-vmvmvp.streamlitapp.com/', 'See the first demo I use on Streemlit', icon_size)
+st_button('linkedin', 'https://www.linkedin.com/in/teresia-buza-11b77a1b/', 'Follow me on LinkedIn', icon_size)
+# st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
+# st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
